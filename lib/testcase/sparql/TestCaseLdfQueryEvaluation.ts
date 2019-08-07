ec8c9c22c0a9561becf517ce6239516105cf9e9c
Manu De Buck <manu.debuck@telenet.be>
2019-08-07 10:32:49 +0200

    Implement resourceToTestCase

--- lib/testcase/sparql/TestCaseLdfQueryEvaluation.ts
+++ lib/testcase/sparql/TestCaseLdfQueryEvaluation.ts
@@ -1,24 +1,74 @@
-import { ITestCaseHandler } from "../ITestCaseHandler";
-import { Resource } from "rdf-object";
-import { ITestCaseData } from "../ITestCase";
-import { IFetchOptions } from "../../Util";
-import { ITestCaseSparql } from "./ITestCaseSparql";
-import { IQueryEngine } from "./IQueryEngine";
-import { TestCaseUnsupported } from "../TestCaseUnsupported";
+import {ITestCaseHandler } from "../ITestCaseHandler";
+import * as RDF from 'rdf-js';
+import {Resource, RdfListMaterializer } from "rdf-object";
+import {ITestCaseData } from "../ITestCase";
+import {IFetchOptions, Util, IFetchResponse } from "../../Util";
+import {ITestCaseSparql } from "./ITestCaseSparql";
+import {IQueryEngine, IQueryResult } from "./IQueryEngine";
+import {TestCaseUnsupported } from "../TestCaseUnsupported";
+import {TestCaseQueryEvaluationHandler} from "./TestCaseQueryEvaluation";
+// tslint:disable:no-var-requires
+const arrayifyStream = require('arrayify-stream');
+const stringifyStream = require('stream-to-string');
+
 
 /**
- * Test case handler for ldf tests.
+ * Test case handler for https://manudebuck.github.io/engine-ontology/engine-ontology.ttl#LdfQueryEvaluationTest
  */
-export class TestCaseLdfQueryEvaluationHandler implements ITestCaseHandler<TestCaseUnsupported> {
+export class TestCaseLdfQueryEvaluationHandler implements ITestCaseHandler<TestCaseLdfQueryEvaluation> {
 
     constructor(){
 
     }
 
-    public async resourceToTestCase(resource: Resource, testCaseData: ITestCaseData, options?: IFetchOptions): Promise<TestCaseUnsupported> {
-        return new TestCaseUnsupported("", testCaseData);
+    public async resourceToTestCase(resource: Resource, testCaseData: ITestCaseData, options?: IFetchOptions): Promise<TestCaseLdfQueryEvaluation> {
+        if(!resource.property.action) {
+            throw new Error(`Missing mf:action in ${resource}`);
+        }
+        if(!resource.property.result) {
+            throw new Error(`Missing mf:reslult in ${resource}`);
+        }
+        const action = resource.property.action;
+        if(!action.property.query){
+            throw new Error(`Missing qt:query in mf:action of ${resource}`);
+        }
+        // Check if Ldf source is stated
+        if(!resource.property.sourceType){
+            throw new Error(`Missing et:sourceType in ${resource}`);
+        }
+        
+        let dataUri: string = null;
+        if(action.property.data){
+            dataUri = action.property.data.value;
+        }
+
+        let queryData: RDF.Quad[] = dataUri ? await arrayifyStream((await Util.fetchRdf(dataUri, options))[1]) : [];
+        const queryResponse = await Util.fetchCached(resource.property.result.value, options);
+        return new TestCaseLdfQueryEvaluation(
+            testCaseData,
+            {
+                baseIRI: Util.normalizeBaseUrl(action.property.query.value),
+                queryString: await stringifyStream((await Util.fetchCached(action.property.query.value, options)).body),
+                queryData: queryData,
+                queryResult: await TestCaseQueryEvaluationHandler.parseQueryResult(
+                    Util.identifyContentType(queryResponse.url, queryResponse.headers),
+                    queryResponse.url, queryResponse.body),
+                resultSource: queryResponse,
+                sourceType: resource.property.sourceType.value
+            }
+        );
     }
 
+}
+
+export interface ILdfTestaseQueryEvaluationProps {
+    baseIRI: string;
+    queryString: string;
+    queryData: RDF.Quad[];
+    queryResult: IQueryResult;
+    resultSource: IFetchResponse;
+    // Necessary for testing different sourceTypes
+    sourceType: string;
 }
 
 export class TestCaseLdfQueryEvaluation implements ITestCaseSparql {
@@ -30,12 +80,20 @@ export class TestCaseLdfQueryEvaluation implements ITestCaseSparql {
     public readonly name: string;
     public readonly uri: string;
 
-    constructor(){
+    public readonly baseIRI: string;
+    public readonly queryString: string;
+    public readonly queryData: RDF.Quad[];
+    public readonly queryResult: IQueryResult;
+    public readonly resultSource: IFetchResponse;
+    public readonly sourceType: string;
 
+    constructor(testCaseData: ITestCaseData, props: ILdfTestaseQueryEvaluationProps){
+        Object.assign(this, testCaseData);
+        Object.assign(this, props);
     }
 
     public async test(engine: IQueryEngine, injectArguments: any): Promise<void> {
-        return null;
+        // TODO: Implement test
     }
     
 }
